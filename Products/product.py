6d48d34dc725b1ad1cb18ed5decca949b85285d1
Maarten <mwi@pop-os.fritz.box>
2025-07-12 18:01:03 +0200

    Scraper 002

--- Products/product.py
+++ Products/product.py
@@ -12,3 +12,11 @@ class ScrapedProduct:
     currency: str = ""
     rating: float = None
     is_sponsored: bool = False
+
+    # Method runs automatically afther dataclass initialization. It trimes whitespace from all string fields. If a string is empty after trimming, it replaces it with a default message like "no name".
+
+    def __post_init_(self):
+        for field in fields (self):
+            value = getattr(self, field.name)
+            if isinstance( value, str):
+                setattr(self, field.name, value.strip() if value.strip() else f" No {field.name}")
