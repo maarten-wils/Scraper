3c4eb2ee5c0d7a2d368f6e2cc07bc6204f323347
Maarten Wils <r0231944@student.thomasmore.be>
2025-07-15 18:38:16 +0200

    Scraper 003

---
+++ README.md
@@ -0,0 +1,5 @@
+To run the Amazon Product Scraper, open a terminal or command prompt, navigate to the folder where your app.py file is located, and run:
+
+streamlit run "app.py"
+
+Streamlit will start a local server on port 8501.
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st      # Streamlit for building the web interface
+import pandas as pd         # Pandas for reading and displaying CSV data
 import os
 from Controllers.scraper_controller import search_products
 from Pipelines.data_pipeline import DataPipeline
 
+
+# Set up the Streamlit app configuration
 st.set_page_config(page_title="Amazon Scraper", page_icon="AS")
 st.title("Amazon Product Scraper")
 product_name = st.text_input("Enther the name of the product to search for: ")
 
+# When the "Scrape" button is clicked, it triggers the scraping process
 if st.button("Scrape"):
     filename = f"{product_name}.csv"
     pipeline = DataPipeline(csv_filename=filename)
@@ -17,8 +20,10 @@ if st.button("Scrape"):
         pipeline.close_pipeline()
         if os.path.exists(filename):
             df = pd.read_csv(filename)
-            st.success("scraping complete")
-            st.dataframe(df)
+            st.success("scraping complete") 
+
+            # Display the scraped data in a table and provide a download button    
+            st.dataframe(df)               
             st.download_button("Download CSV", df.to_csv(index=False), file_name=filename, mime="text/csv")
         else:
             st.warning("no results were found")
